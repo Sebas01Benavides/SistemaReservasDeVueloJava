cc6e62cedd8ccf780ae44ac40bf3e26e0111dae3
Sebas <Sebas@LAPTOPSB.home.lla.com>
2025-08-04 22:22:55 -0600

    Creacion clase Config

---
+++ src/sistemavuelos/SistemaVuelos.java
@@ -0,0 +1,13 @@
+
+package sistemavuelos;
+
+public class SistemaVuelos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
